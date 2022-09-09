c824f695d3400a41eba7bca64552dce1b027a521
Nick Koukis <nikoskoukis99@gmail.com>
2022-09-09 20:29:57 +0300

    Fix Links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = () => {
   return (
     <footer>
       <ul className='permalinks'>
-        <li><a href="!#">Home</a></li>
-        <li><a href="!#about">About</a></li>
-        <li><a href="!#experience">Experience</a></li>
-        <li><a href="!#skills">Skills</a></li>
-        <li><a href="!#portfolio">Portfolio</a></li>
-        <li><a href="!#contact">Contact</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className="footer__socials">
