82096c3550f6ee747cb0e7316b37bcc0713ef624
Nick Koukis <nikoskoukis99@gmail.com>
2022-09-10 00:11:36 +0300

    test form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -24,12 +24,23 @@ const Contact = () => {
             <a href={process.env.REACT_APP_MESSENGER} target='_blank' rel="noopener noreferrer">Send a message</a>
           </article>
         </div>
-        <form name="contact" method='POST' netlify >
+        {/* <form name="contact" method='POST' netlify >
           <input type="hidden" name="form-name" value="contact" />
           <input type="text" name="name" placeholder="Your Full Name" required />
           <input type="email" name="email" placeholder="Your Email" required />
           <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
           <button type="submit" className='btn btn-primary' id='contactBtn'>Send Message</button>
+        </form> */}
+        <form name="contact" netlify>
+          <p>
+            <label>Name <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Email <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
       </div>
     </section>
